e503ebf5dd50102366716b3ee13b8d73f086e0e1
GabrielFardoux <86362289+GabrielFardoux@users.noreply.github.com>
2024-09-16 21:02:51 +0200

    Add files via upload

---
+++ TPperso/target/classes/org/example/Manga.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Manga {
+
+    private String name;
+    private int tome;
+    private boolean acquis;
+
+    public Manga(java.lang.String name, int tome,boolean acquis) {
+        this.name = name;
+        this.tome = tome;
+        this.acquis = acquis;
+    }
+    public boolean getAcquis(){
+        return acquis;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getTome(){
+        return tome;
+    }
+    public String findName(String name){ return name;}
+
+
+
+    @Override
+    public String toString() {
+        return this.name+ " " + this.tome + " " + this.acquis;
+    }
+
+
+}
